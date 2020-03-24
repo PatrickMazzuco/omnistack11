d0c6936fd54e130927981a48b94b9cca545a66b1
PatrickMazzuco <patrick.flores@acad.pucrs.br>
2020-03-24 14:12:54 -0300

    Add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 import routes from './routes';
 
 const app = express();
 
 // Init middleware
+app.use(cors());
 app.use(express.json());
 
 // Init routes
