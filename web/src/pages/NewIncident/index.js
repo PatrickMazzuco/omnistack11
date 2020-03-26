fd8a7640c524c8da94ed3fb83420a25ffb0c576f
PatrickMazzuco <patrick.flores@acad.pucrs.br>
2020-03-26 14:55:14 -0300

    Hook incident creation to backend

--- web/src/pages/NewIncident/index.js
+++ web/src/pages/NewIncident/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import logoImg from '../../assets/logo.svg';
+import api from '../../services/api';
 
 import './styles.css';
 
 const NewIncident = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+
+  const history = useHistory();
+  const ongId = localStorage.getItem('ongId');
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const data = { title, description, value };
+
+    try {
+      await api.post('/incidents', data, {
+        headers: {
+          Authorization: ongId
+        }
+      });
+
+      history.push('/profile');
+    } catch (err) {
+      alert('Erro ao cadastrar caso. Tente novamente.');
+    }
+  };
+
   return (
     <div className="new-incident-container">
       <div className="content">
@@ -23,10 +48,24 @@ const NewIncident = () => {
             Voltar
           </Link>
         </section>
-        <form>
-          <input placeholder="Título do caso" type="text" />
-          <textarea placeholder="Título do caso" />
-          <input placeholder="Valor em reais" type="text" />
+        <form onSubmit={handleSubmit}>
+          <input
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            placeholder="Título do caso"
+            type="text"
+          />
+          <textarea
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+            placeholder="Descrição do caso"
+          />
+          <input
+            value={value}
+            onChange={e => setValue(e.target.value)}
+            placeholder="Valor em reais"
+            type="text"
+          />
 
           <button className="button" type="submit">
             Cadastrar
