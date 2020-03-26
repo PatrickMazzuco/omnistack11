0b3b86117b2a3187a96af88a287d4256999aea93
PatrickMazzuco <patrick.flores@acad.pucrs.br>
2020-03-26 11:47:23 -0300

    Add router

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import Login from './pages/Login';
+import Routes from './routes';
 
 import './global.css';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Routes />
     </div>
   );
 }
