170adb48097922a0f0be24536338ae29b071ec33
PatrickMazzuco <patrick.flores@acad.pucrs.br>
2020-03-26 13:52:29 -0300

    Hook login page to backend

--- web/src/pages/Login/index.js
+++ web/src/pages/Login/index.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiLogIn } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
+import api from '../../services/api';
 
 import './styles.css';
 
 const Login = () => {
+  const [id, setId] = useState('');
+
+  const history = useHistory();
+
+  const handleLogin = async e => {
+    e.preventDefault();
+
+    try {
+      const response = await api.post('/auth/login', { id });
+
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+
+      history.push('/profile');
+    } catch (err) {
+      alert('Falha no login. Tente novamente.');
+    }
+  };
+
   return (
     <div className="login-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero" />
 
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Login</h1>
-          <input placeholder="Seu ID" type="text" />
+          <input
+            value={id}
+            onChange={e => setId(e.target.value)}
+            placeholder="Seu ID"
+            type="text"
+          />
           <button className="button" type="submit">
             Entrar
           </button>
